c717b697252e65b37e2cb9844fc51d4fea527f86
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-07-25 18:10:59 +0300

    add hide and show effect on what we do

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-  $(".designicon").click(function(){
+  $("#design").click(function(){
     $("#designp").show();
-    $(".designicon").hide();
+    $("#design").hide();
+  });
+  $("#designp").click(function(){
+    $("#design").show();
+    $("#designp").hide();
+  });
+  $("#devicon").click(function(){
+    $("#dev").show();
+    $("#devicon").hide();
+  });
+  $("#dev").click(function(){
+    $("#devicon").show();
+    $("#dev").hide();
+  });
+  $("#production").click(function(){
+    $("#productp").show();
+    $("#production").hide();
+  });
+  $("#productp").click(function(){
+    $("#production").show();
+    $("#productp").hide();
   });
 });
